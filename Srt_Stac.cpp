aa42df6cd29ecc9594c53f7fb82dd9562229b744
Hritik Kumar <hritikkumar@Hritiks-MacBook-Air.local>
2025-10-03 16:17:31 +0530

    Your message

---
+++ Srt_Stac.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+void insert(stack<int> &st, int k)
+{
+    int n = st.size();
+    if (n == 0 || st.top() <= k)
+    {
+        st.push(k);
+        return;
+    }
+    int val = st.top();
+    st.pop();
+    insert(st, k);
+    st.push(val);
+}
+void sort(stack<int> &st)
+{
+    int n = st.size();
+    if (n == 0 || n == 1)
+        return;
+    int temp = st.top();
+    st.pop();
+    sort(st);
+    insert(st, temp);
+}
+int main()
+{
+    stack<int> st;
+    int n;
+    cout << "enter the size of stack";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int k;
+        cin >> k;
+        st.push(k);
+    }
+    sort(st);
+    cout << "\nsorted stack in descending order:\n";
+
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    return 0;
+}
